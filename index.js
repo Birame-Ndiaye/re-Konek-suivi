0f2a277ea72a5c7973c852eb36ae42dffe1de046
Ousseynou19 <108348767+Ousseynou19@users.noreply.github.com>
2024-10-22 16:53:41 +0200

    index modifier pour le deploiement

--- index.js
+++ index.js
@@ -376,6 +376,12 @@ app.get('/repairList/:clientId', (req, res) => {
 
 
 // Démarrer le serveur Express
-app.listen(3000, () => {
-  console.log("Serveur démarré sur le port 3000");
+// app.listen(3000, () => {
+//   console.log("Serveur démarré sur le port 3000");
+// });
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
